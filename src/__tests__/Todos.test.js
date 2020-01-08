3c23a3237daebce85d1525d735381e1fb78417fe
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-01-08 15:54:39 -0700

    Merged

--- src/__tests__/Todos.test.js
+++ src/__tests__/Todos.test.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import { render, act } from '@testing-library/react'
 import axios from 'axios'
-import Todo from '../Components/Todo'
+import Todos from '../Components/Todos'
