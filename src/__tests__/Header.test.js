986ed226ad8bee6366cc7c32d234bfe6e9118109
Eric Sellors <github@esellors.com>
2020-03-25 13:12:34 -0500

    added further descriptions for todos.test

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import Header from '../Components/Header'
 
-// Have React Testing Library open during testing: https://testing-library.com/docs/intro
-
 it('Does not show dropdown when mounted', () => {
   const { queryByTestId } = render(<Header />)
   const dropdown = queryByTestId('dropdown')
--- src/__tests__/Todos.test.js
+++ src/__tests__/Todos.test.js
@@ -3,22 +3,26 @@ import { render, act } from '@testing-library/react'
 import axios from 'axios'
 import Todos from '../Components/Todos'
 
-// Have React Testing Library open during testing: https://testing-library.com/docs/intro
-
 it('Renders todos', async () => {
-  let component;
+    let component;
+  
+    // React Testing works with other testing libraries
+    // This is us using Jest with React Testing
+    // jest is a global object that is automatically in scope with every test file created
 
-  jest.spyOn(axios, 'get').mockImplementation(() =>
-    Promise.resolve({
-      data: [{ id: 1, title: 'test title' }],
+    // We implement mockImplementation in order to utilize axios
+    // More on mock functions at https://jestjs.io/docs/en/mock-function-api.html
+    jest.spyOn(axios, 'get').mockImplementation(() =>
+      Promise.resolve({
+        data: [{ id: 1, title: 'test title' }],
+      })
+    )
+  
+    await act(async () => {
+      const { container } = render(<Todos />)
+      component = container;
+      //Grabs the dom container from the Todos component and reassigns it to the component variable
     })
-  )
-
-  await act(async () => {
-    const { container } = render(<Todos />)
-    component = container;
-    //Grabs the dom container from the Todos component and reassigns it to the component variable
+  
+    expect(component.textContent).toContain('test title')
   })
-
-  expect(component.textContent).toContain('test title')
-})
